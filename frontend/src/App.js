3d8f24db6ff30a066922699ee1250c265a0d2a1d
mohamed yasser <mohamedyasser12346262@gmail.com>
2024-02-05 20:00:24 +0200

    update project19

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import react from 'react'
 import {BrowserRouter, NavLink,Route,Routes} from 'react-router-dom'
+import M from './component/m'; 
+import Emp from './component/emp'; 
 import Navbar from './component/Navbar'
 import Cv from './component/Cv'; 
 import Cv2 from './component/CV2'; 
-import M from './component/m'; 
-import Emp from './component/emp'; 
 import Home from './component/home';
 function App() {
   return (
@@ -12,7 +12,8 @@ function App() {
     <div className="App">
       <Routes>
       <Route path='/m' element={<M/>}/> 
-        <Route path='/emp' element={<Emp/>}/> 
+      <Route path='/emp' element={<Emp/>}/> 
+
         <Route path='/' element={
           <div>
           <Navbar/>
