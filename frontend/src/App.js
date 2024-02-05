e457fb8f76e89f5409da7d5beecfaa574d337279
mohamed yasser <mohamedyasser12346262@gmail.com>
2024-02-05 19:50:21 +0200

    update project17

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,6 @@ import M from './component/m';
 import Emp from './component/emp'; 
 import './App.css';
 import Home from './component/home';
-import Navbar from './component/Navbar';
 function App() {
   return (
     <BrowserRouter>
@@ -14,24 +13,9 @@ function App() {
       {/* <h1 className="k">Home page</h1> */}
       
       <Routes>
-        <Route path='/' element={
-          <div>
-        <Navbar/>
-        <Home/>
-        </div>
-        }/>
-        <Route path='/Cv' element={
-          <div>
-        <Navbar/>
-        <Cv/>
-        </div>
-        }/>
-        <Route path='/support' element={
-          <div>
-        <Navbar/>
-        <Cv2/>
-        </div>
-        }/>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/Cv' element={<Cv/>}/>
+        <Route path='/Cv2' element={<Cv2/>}/>
         <Route path='/m' element={<M/>}/> 
         <Route path='/emp' element={<Emp/>}/> 
       </Routes>
