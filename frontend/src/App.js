46b52da0a140d630eb677b86675c135fee0370d5
mohamed yasser <mohamedyasser12346262@gmail.com>
2024-02-05 19:56:48 +0200

    update project18

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,37 @@
 import react from 'react'
 import {BrowserRouter, NavLink,Route,Routes} from 'react-router-dom'
+import Navbar from './component/Navbar'
 import Cv from './component/Cv'; 
 import Cv2 from './component/CV2'; 
 import M from './component/m'; 
 import Emp from './component/emp'; 
-import './App.css';
 import Home from './component/home';
 function App() {
   return (
     <BrowserRouter>
     <div className="App">
-      {/* <h1 className="k">Home page</h1> */}
-      
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/Cv' element={<Cv/>}/>
-        <Route path='/Cv2' element={<Cv2/>}/>
-        <Route path='/m' element={<M/>}/> 
+      <Route path='/m' element={<M/>}/> 
         <Route path='/emp' element={<Emp/>}/> 
+        <Route path='/' element={
+          <div>
+          <Navbar/>
+        <Home/>
+        </div>
+        }/>
+        <Route path='/Cv' element={
+          <div>
+          <Navbar/>
+        <Cv/>
+        </div>
+        }/>
+        <Route path='/Cv2' element={
+          <div>
+          <Navbar/>
+        <Cv2/>
+        </div>
+        }/>
+       
       </Routes>
 
     </div>
