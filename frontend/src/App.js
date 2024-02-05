7c0cd6a3dc2acbc3732ed4f11c47cf05e6691036
mohamed yasser <mohamedyasser12346262@gmail.com>
2024-02-06 01:42:17 +0200

    update project36

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,64 @@
-// frontend/src/App.js
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-
+import Cv from './component/Cv'; 
+import Cv2 from './component/CV2'; 
 import Login from './component/emp';
 import Home from './component/m';
+import Navbar from  "./component/Navbar";
 import './App.css';
-
+import Home2 from './component/home';
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
 
   const handleLogin = () => {
     setLoggedIn(true);
   };
-
   return (
-    <Router>
+    <div className="App">
+      {/* <h1 className="k">Home page</h1> */}
+      <Router>
       <Routes>
         <Route
-          path="/login"
+          path="/emp"
           element={loggedIn ? <Navigate to="/home" /> : <Login onLogin={handleLogin} />}
         />
         <Route
           path="/home"
           element={loggedIn ? <Home /> : <Navigate to="/login" />}
         />
+         <Route path='/' element={
+          <div>
+            <Navbar/>
+         <Home2/>
+         </div>
+         }/>
+        <Route path='/Cv' element={
+          <div>
+          <Navbar/>
+        <Cv/>
+        </div>
+        }/>
+        <Route path='/support' element={
+        <div>
+        <Navbar/>
+        <Cv2/>
+        </div>
+        }/>
       </Routes>
     </Router>
+      
+      {/* <Routes>
+        <Route path='/' element={<Home2/>}/>
+        <Route path='/Cv' element={<Cv/>}/>
+        <Route path='/Cv2' element={<Cv2/>}/>
+        <Route path='/m' element={<M/>}/> 
+        <Route path='/emp' element={<Emp/>}/> 
+      </Routes> */}
+
+    </div>
   );
 }
 
 export default App;
+
+
--- frontend/src/component/emp.js
+++ frontend/src/component/emp.js
@@ -1,9 +1,10 @@
-// frontend/src/Login.js
 import React, { useState } from 'react';
-
+import style from "./css/login.module.css"
 function Login({ onLogin }) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+
+ 
 
   const handleLogin = async () => {
     try {
@@ -27,17 +28,15 @@ function Login({ onLogin }) {
   };
 
   return (
-    <div className="App">
-      <h1>Login</h1>
-      <div>
-        <label>Username:</label>
-        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-      </div>
-      <div>
-        <label>Password:</label>
-        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      </div>
-      <button onClick={handleLogin}>Login</button>
+    <div className={style.container}>
+      <form className={style.card}>
+       
+            <h1>Login</h1>
+            <input type="text" placeholder='Username' value={username} onChange={(e) => setUsername(e.target.value)} />
+            <input type="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
+            <button onClick={handleLogin}>Login</button>
+       
+      </form>
     </div>
   );
 }
