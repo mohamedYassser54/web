f64b660359c14e8393e9f191c258cd36f48886b5
mohamed yasser <mohamedyasser12346262@gmail.com>
2024-02-14 00:58:38 +0200

    update project53

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(cookieParser()); 
 dotenv.config()
 app.use(cors({
-  origin: 'https://web-beta-woad.vercel.app',
+  origin: 'https://server-three-mauve-23.vercel.app/',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   headers: ['Content-Type', 'Authorization'],
