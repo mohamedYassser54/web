9d8c0f0d0365fd19b2130e1839643fb02d03fd1b
mohamed yasser <mohamedyasser12346262@gmail.com>
2024-02-14 11:07:57 +0200

    update project69

--- backend/index.js
+++ backend/index.js
@@ -162,11 +162,9 @@ app.post("/employees", upload.single('cv'), (req, res) => {
 
 
 // getdata
-app.get('/get', (req, res) => {
-  const sql = 'SELECT * FROM `employees` WHERE password = ? AND name = ?';
-  // const values = ['s2as2', 'hr'];
-
-  db.query(sql,  (err, data) => {
+app.get('/get',  (req, res) => {
+  const sql = 'SELECT * FROM `employees`';
+  db.query(sql, (err, data) => {
     if (err) {
       return res.status(500).json({ message: 'Internal Server Error' });
     }
@@ -181,7 +179,6 @@ app.get('/get', (req, res) => {
     return res.json(formattedData);
   });
 });
-
 
 
 // m
